22bcd1fa19e13cf02530928e46d2cd0ad4354752
FraMa <franky.ma24@gmail.com>
2020-10-20 14:49:50 +0100

    Returns sorted bus data

---
+++ BusBoard.ConsoleApp/JsonItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BusBoard.ConsoleApp
+{
+    class JsonItem : IComparable<JsonItem>
+    {
+        [JsonProperty("expectedArrival")]
+        private readonly DateTime expectedArrival;
+        [JsonProperty("lineName")]
+        private readonly string lineName;
+        [JsonProperty("destinationName")]
+        private readonly string destinationName;
+
+        public override string ToString()
+        {
+            return $"{expectedArrival}, {lineName}, {destinationName}";
+        }
+
+        public int CompareTo(JsonItem item)
+        {
+            return this.expectedArrival.CompareTo(item.expectedArrival);
+        }
+    }
+
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace BusBoard.ConsoleApp
-{
-  class Program
-  {
-    static void Main(string[] args)
+{ 
+    class Program
     {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient("https://api.tfl.gov.uk/");
+
+            Console.Write("Stop code: ");
+            var stopCode = Console.ReadLine();
+
+            var request = new RestRequest($"StopPoint/{stopCode}/Arrivals", DataFormat.Json);
+            var response = client.Get(request);
+            var jsonItems = JsonConvert.DeserializeObject<List<JsonItem>>(response.Content);
+
+            jsonItems.Sort();
+            jsonItems = jsonItems.GetRange(0, 5);
+
+            foreach (var item in jsonItems)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
     }
-  }
 }
